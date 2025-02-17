e3e07365c1249d9e3ded50b0a00d6c90ec0f6fbe
nodong91 <nodong91@gmail.com>
2025-02-17 19:06:01 +0900

    sadas

--- Scripts/CustomTimeline/CT_Cinemachine_Track.cs
+++ Scripts/CustomTimeline/CT_Cinemachine_Track.cs
@@ -4,9 +4,6 @@ using UnityEngine.Timeline;
 namespace P01.Editor
 {
     [TrackBindingType(typeof(CT_Cinemachine_Receiver))]
-    [TrackColor(1f, 1f, 0f)]
-    public class CT_Cinemachine_Track : MarkerTrack
-    {
-
-    }
+    [TrackColor(1f, 0f, 1f)]
+    public class CT_Cinemachine_Track : MarkerTrack { }
 }
--- Scripts/CustomTimeline/CT_ShaderControl_Track.cs
+++ Scripts/CustomTimeline/CT_ShaderControl_Track.cs
@@ -6,7 +6,7 @@ namespace P01.Editor
 {
     [TrackBindingType(typeof(CT_ShaderControl_Receiver))]
     [TrackClipType(typeof(CT_ShaderControl_Clip))]
-    [TrackColor(1f, 1f, 0f)]
+    [TrackColor(1f, 0f, 1f)]
     public class CT_ShaderControl_Track : TrackAsset // 트랙 만들기
     {
         public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
