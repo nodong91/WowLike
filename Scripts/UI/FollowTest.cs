5c13006d04becd76977579385ac48a8292b697fd
nodong91 <nodong91@gmail.com>
2025-03-19 19:29:47 +0900

    sadasda

--- Scripts/UI/FollowTest.cs
+++ Scripts/UI/FollowTest.cs
@@ -6,42 +6,30 @@ public class FollowTest : MonoBehaviour
 {
     public Transform image;
     public Transform target;
-    public Camera uiCam;
+    public Camera uiCamera;
     public Vector3 offset;
-
-    void Start()
-    {
-        FollowStruct followTarget = new FollowStruct(target, offset);
-        AddFollowUI_UICamera(image, followTarget, uiCam);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     public struct FollowStruct
     {
         public Transform followTarget;
         public Vector3 followOffset;
+        public bool camera;
 
-        public FollowStruct(Transform _target, Vector3 _offset)
+        public FollowStruct(Transform _target, Vector3 _offset, bool _camera)
         {
             followTarget = _target;
             followOffset = _offset;
+            camera = _camera;
         }
     }
     public Dictionary<Transform, FollowStruct> dictFollow = new Dictionary<Transform, FollowStruct>();
     Coroutine followUI;
-    //public Transform testUI;
+
+    void Start()
+    {
+        FollowStruct followTarget = new FollowStruct(target, offset, true);
+        AddFollowUI(image, followTarget);
+    }
 
     public void AddFollowUI(Transform _addFollow, FollowStruct _addStruct)
     {
@@ -83,37 +71,15 @@ public class FollowTest : MonoBehaviour
             foreach (var child in _follows)
             {
                 Vector3 screenPosition = Camera.main.WorldToScreenPoint(child.Value.followTarget.position + child.Value.followOffset);
-                child.Key.position = screenPosition;
-            }
-            yield return null;
-        }
-    }
-
-    public void AddFollowUI_UICamera(Transform _addFollow, FollowStruct _addStruct, Camera _uiCam)
-    {
-        if (dictFollow.ContainsKey(_addFollow) == false)
-        {
-            dictFollow.Add(_addFollow, _addStruct);
-            StartFollowing_UICamera(_uiCam);
-        }
-    }
-
-    void StartFollowing_UICamera(Camera _uiCam)
-    {
-        if (followUI != null)
-            StopCoroutine(followUI);
-        followUI = StartCoroutine(StartFollowing_UICamera(dictFollow, _uiCam));
-    }
-
-    IEnumerator StartFollowing_UICamera(Dictionary<Transform, FollowStruct> _follows, Camera _uiCam)
-    {
-        while (dictFollow.Count > 0)
-        {
-            foreach (var child in _follows)
-            {
-                Vector3 screenPosition = Camera.main.WorldToScreenPoint(child.Value.followTarget.position + child.Value.followOffset);
-                Vector3 followPosition = _uiCam.ScreenToWorldPoint(screenPosition);
-                child.Key.position = followPosition;
+                if (child.Value.camera == true)// 카메라 캔버스인 경우
+                {
+                    Vector3 followPosition = uiCamera.ScreenToWorldPoint(screenPosition);
+                    child.Key.position = followPosition;
+                }
+                else
+                {
+                    child.Key.position = screenPosition;
+                }
             }
             yield return null;
         }
