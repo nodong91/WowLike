083916a4634f388ecc93237c5e5d41450dbf73a2
nodong91 <nodong91@gmail.com>
2025-05-20 20:08:01 +0900

    asdasd

--- 00_Test/TimeScaleTest.cs
+++ 00_Test/TimeScaleTest.cs
@@ -2,18 +2,16 @@ using UnityEngine;
 
 public class TimeScaleTest : MonoBehaviour
 {
-    public float unscaledTime, timer;
+    public float unscaledTime;
     public float timeScale;
 
     void Update()
     {
-        timeScale = Time.timeScale;
-        if (Input.GetMouseButtonDown(1))
-        {
-            Time.timeScale = Time.timeScale > 0f ? 0f : 1f;
-            if (Time.timeScale == 0)
-                timer = Time.time;
-        }
+        //timeScale = Time.timeScale;
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    Time.timeScale = Time.timeScale > 0f ? 0f : 1f;
+        //}
         if (Time.timeScale == 0f)
         {
             Time.timeScale = 0f;
