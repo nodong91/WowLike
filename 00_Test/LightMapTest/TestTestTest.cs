d7d5d0afb080b16cc58a21edaaa114fe3e103fec
nodong91 <nodong91@gmail.com>
2025-05-27 19:29:30 +0900

    adsfasd

--- 00_Test/LightMapTest/TestTestTest.cs
+++ 00_Test/LightMapTest/TestTestTest.cs
@@ -1,15 +1,30 @@
-using NUnit.Framework.Internal;
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TestTestTest : MonoBehaviour
 {
     public SettingLightMap instance;
     SettingLightMap test;
+
+    public List<Light> lightList = new List<Light>();
+
     void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
-            test = Instantiate(instance);
+            //test = Instantiate(instance);
+            Light[] lights = (Light[])FindObjectsByType(typeof(Light), FindObjectsSortMode.None);
+            for (int i = 0; i < lights.Length; i++)
+            {
+                Light temp = lights[i];
+                bool Realtime = temp.lightmapBakeType == LightmapBakeType.Realtime;
+                bool Directional = temp.type == LightType.Directional;
+                if (Realtime == true && Directional == true)
+                {
+                    lightList.Add(temp);
+                }
+            }
         }
     }
 }
